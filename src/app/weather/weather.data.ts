3d3c127cd90863d5dce196111a47e8c00d37fcea
shirleytruffierblanc <shirley.truffier_blanc@yahoo.com>
2018-07-20 09:55:52 -0500

    first commit

--- src/app/weather/weather.data.ts
+++ src/app/weather/weather.data.ts
@@ -1,6 +1,6 @@
 import {Weather} from "./weather";
 export const WEATHER_LIST: Weather[] = [
-    new Weather('Japan', 'CLoudy', 34, 2, '01d'),
-    new Weather('China', 'Rainy', 24, 2 , '01d'),
-    new Weather('IN', 'Clear', 14, 2, '01d'),
+    new Weather('Japan', 'CLoudy', 34, 2),
+    new Weather('China', 'Rainy', 24, 2 ),
+    new Weather('IN', 'Clear', 14, 2),
 ];
--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -3,12 +3,11 @@ export class Weather{
               public city: string, 
               public description: string,
               public temperature: number,
-              public  date:number,
-              public icon : string    ){
+              public  date:number    ){
                     this.city = city;
                     this.description = description;
                     this.temperature = temperature;
                     this.date = date;
-                    this.icon=icon;
+                    
               } 
 }
